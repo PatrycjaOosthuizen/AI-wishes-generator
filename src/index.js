e2f15b85a4f7ba2e1ed80b60d78f1d4f836312a8
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-02-13 05:01:08 +0000

    Added basic form handling and added Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateWishes(event) {
+  event.preventDefault();
 
+  new Typewriter("#wishes", {
+    strings: "WISHES GOES HERE",
+    autoStart: true,
+    delay: 3,
+    cursor: "",
+  });
+}
+
+let wishesFormatElement = document.querySelector("#wishes-generator-form");
+wishesFormatElement.addEventListener("submit", generateWishes);
