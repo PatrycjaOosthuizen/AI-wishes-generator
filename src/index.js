3812bee50770865ce82d223f9c1ca9dba00b0670
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-02-14 05:24:10 +0000

    Added in Java Script object trim and toUppercase

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ function generateWishes(event) {
   let toneSelect = document.querySelector("#tone");
   let occasionSelect = document.querySelector("#occasion");
   let apiKey = "6f64aatd0b0oe3cc63e4fb944c32303a";
-  let prompt = `User selected and inputed instructions: Generate ${occasionSelect.value} wishes for a ${forSelect.value} set tone of the wishes to ${toneSelect.value} write wishes are dedicated to ${nameInput.value}`;
+  let prompt = `User selected and inputed instructions: Generate ${
+    occasionSelect.value
+  } wishes for a ${forSelect.value} set tone of the wishes to ${
+    toneSelect.value
+  } write wishes are dedicated to ${nameInput.value.trim().toUpperCase()}`;
   let context =
     "You are greetings and occasions wishes expert and love to write short beautiful wishes. Your mission is to write short 4 lines wishes in basic HTML. Please always seperate each sentence to new line. Make sure to follow user selected and inputed instructions. Do not include a title to the wishes. Sign the wishes </br> </br> <strong>SheCodes AI</strong> at the end of the wishes.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
